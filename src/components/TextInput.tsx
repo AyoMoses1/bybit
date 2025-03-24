cdd7b6f830bbb233c2ad7042a037d650245fd459
RitaDee <ritankemdaniel@gmail.com>
2025-03-24 14:42:09 +0100

    feat: add optional id prop to TextInput component

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,6 +1,7 @@
 import React, { forwardRef } from "react";
 
 interface TextInputProps {
+  id?: string | number;
   label: string;
   value?: string;
   placeholder?: string;
@@ -10,17 +11,18 @@ interface TextInputProps {
 }
 
 const TextInput = forwardRef<HTMLInputElement, TextInputProps>(
-  ({ label, disabled, type, placeholder, ...props }, ref) => {
+  ({ id, label, disabled, type, placeholder, ...props }, ref) => {
     return (
       <div className="w-full">
         <label
-          className={`block font-[Roboto] text-sm font-normal ${
+          className={`block font-[Roboto] text-base font-normal ${
             disabled ? "text-[#A2A9B0]" : "text-[#21272A]"
           }`}
         >
           {label}
         </label>
         <input
+          id={id?.toString()}
           type={type}
           disabled={disabled}
           className={`mt-1 h-[48px] w-full border-b-[1.5px] px-4 py-2 outline-none ${disabled ? "border-gray-300 bg-[#F8F8F8] text-gray-500" : "border-[#C1C7CD] bg-[#F8F8F8]"}`}
