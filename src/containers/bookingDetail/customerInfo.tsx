0f670793036e85ee77f8207319f051759f60a1f2
RitaDee <ritankemdaniel@gmail.com>
2025-03-26 10:55:20 +0100

    feat: replace img with Image component for customer profile picture

--- src/containers/bookingDetail/customerInfo.tsx
+++ src/containers/bookingDetail/customerInfo.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 import { BookingType } from "./bookingTypes";
 
 const CustomerInformation = ({ customer }: { customer: BookingType }) => {
@@ -11,17 +12,21 @@ const CustomerInformation = ({ customer }: { customer: BookingType }) => {
       <div className="mb-6 flex flex-col items-center">
         <div className="mb-3 h-20 w-20 overflow-hidden rounded-full bg-gray-200">
           {customer.customer_image ? (
-            <img
+            <Image
               src={customer.customer_image}
               alt={customer.customer_name || "Customer"}
               className="h-full w-full object-cover"
+              width={40}
+              height={40}
             />
           ) : (
             <div className="flex h-full w-full items-center justify-center">
-              <img
+              <Image
                 src="https://placehold.co/80x80"
                 alt="Customer"
                 className="h-full w-full object-cover"
+                width={40}
+                height={40}
               />
             </div>
           )}
