621b257deb964eba7b95973095b0f072665ca248
AyoMoses1 <ayomoses111@gmail.com>
2025-03-26 12:52:42 +0100

    resolve client errors

--- src/app/update-promo/page.tsx
+++ src/app/update-promo/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { useGetPromoById, useUpdatePromo } from "@/lib/api/hooks/usePromo";
