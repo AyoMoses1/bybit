ebdf5ec0be0590641c47a084af973333be487772
olan-tayo <olantayo3627@gmail.com>
2025-03-26 12:48:25 +0100

    feat:implement withdrawal feature

--- src/lib/api/apiHandlers/withdrawalService.ts
+++ src/lib/api/apiHandlers/withdrawalService.ts
@@ -1,7 +1,8 @@
+import { WithdrawalType } from "@/containers/withdrawals";
 import firebase from "@/lib/firebase";
-import { onValue } from "firebase/database";
+import { child, onValue, update } from "firebase/database";
 
-export const fetchWithdrawal = () => {
+export const fetchWithdrawal = (search: string) => {
   return new Promise((resolve, reject) => {
     try {
       const { withdrawRef } = firebase;
@@ -9,15 +10,50 @@ export const fetchWithdrawal = () => {
       onValue(withdrawRef, (snapshot) => {
         if (snapshot.val()) {
           const data = snapshot.val();
-          const arr = Object.keys(data).map((i) => {
-            data[i].id = i;
-            return data[i];
-          });
+          const arr = Object.keys(data)
+            .map((i) => {
+              data[i].id = i;
+              return data[i];
+            })
+            .filter(
+              (withdrawal) =>
+                !search ||
+                withdrawal.id?.toLowerCase().includes(search.toLowerCase()) ||
+                withdrawal.name?.toLowerCase().includes(search.toLowerCase()) ||
+                withdrawal.amount?.toString().includes(search.toLowerCase()) ||
+                withdrawal.bankName
+                  ?.toLowerCase()
+                  .includes(search.toLowerCase()) ||
+                withdrawal.bankCode
+                  ?.toLowerCase()
+                  .includes(search.toLowerCase()) ||
+                withdrawal.bankAccount
+                  ?.toLowerCase()
+                  .includes(search.toLowerCase()),
+            );
           resolve(arr.reverse());
         } else {
           resolve("No WITHDRAWS available.");
         }
       });
+    } catch (error) {
+      console.error("Fetch Withdrawals Error:", error);
+      reject(error);
+    }
+  });
+};
+
+export const processWithdrawal = (data: WithdrawalType) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const { withdrawRef } = firebase;
+
+      update(child(withdrawRef, data.id), {
+        ...data,
+        processed: true,
+        processDate: new Date().getTime(),
+      });
+      resolve("Processed Successfully");
     } catch (error) {
       console.error("Fetch Withdrawals Error:", error);
       reject(error);
--- src/lib/api/hooks/withdrawals.ts
+++ src/lib/api/hooks/withdrawals.ts
@@ -1,13 +1,34 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchWithdrawal } from "../apiHandlers/withdrawalService";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import {
+  fetchWithdrawal,
+  processWithdrawal,
+} from "../apiHandlers/withdrawalService";
+import { WithdrawalType } from "@/containers/withdrawals";
+import toast from "react-hot-toast";
 
 const USER_STATE_KEY = "withdrawals";
 
 export const useWithdrawals = (search?: string) => {
   return useQuery({
     queryKey: [USER_STATE_KEY, search],
-    queryFn: () => fetchWithdrawal(search),
+    queryFn: () => fetchWithdrawal(search ?? ""),
     staleTime: Infinity,
     retry: 2,
+  });
+};
+export const useProcessWithdrawal = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ data }: { data: WithdrawalType }) => processWithdrawal(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [USER_STATE_KEY] });
+      toast.success("Withdrawal has been processed successfully!");
+    },
+
+    onError: (error) => {
+      console.error("Car deletion failed:", error);
+      toast.error("Failed to process withdrawal. Please try again.");
+    },
   });
 };
